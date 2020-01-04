0c2d285eb2cc8daecee0d05bb773670e78fc5c4c
moon-grp <olumidemm@gmail.com>
2020-01-04 21:28:08 +0100

    starting new portfolio built with nuxt

---
+++ layouts/default.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app class="body">
+  <Nuxt/>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+.body {
+  min-height: 100vh;
+  border-style: double;
+  border-color: #bffcf9;
+  border-width:5px;
+  background-color: #fff;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-container class="pt-12">
+    <v-row class="mt-12">
+      <div class="font_nav">
+        Experience
+      </div>
+    </v-row>
+    <v-row class="mt-2">
+      <div class="font_nav">
+        Projects
+      </div>
+    </v-row>
+    <v-row class="pusherT font_name">
+      <div>
+        Olumide Adebayo
+      </div>
+    </v-row>
+    <v-row>
+      <div class="font_style">
+        Frontend developer with 2+ years of experience. <br />
+        I create innovative, accessible and fast webapps using vuejs.
+      </div>
+    </v-row>
+    <v-row class="pt-2 ml-n3">
+      <v-btn href="http://github.com/moon-grp" tile x-large color="#ccc" text icon>
+        <v-icon>mdi-github-circle</v-icon>
+      </v-btn>
+      <v-btn href="https://twitter.com/GrpMoon" tile x-large color="#ccc" text icon>
+        <v-icon>mdi-twitter</v-icon>
+      </v-btn>
+      <v-btn href="https://www.linkedin.com/in/olumide-adebayo-263321158/" tile x-large color="#ccc" text icon>
+        <v-icon>mdi-linkedin</v-icon>
+      </v-btn>
+    </v-row>
+    <v-row>
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Baloo+Bhai|Oswald:300,400&display=swap');
+.font_nav {
+  font-family: 'Oswald', sans-serif;
+  font-size: 25px;
+  color: #ec87e4;
+}
+.font_name {
+  font-family: 'Baloo Bhai', cursive;
+  font-size: 47px;
+  color: #ec87e4;
+  font-weight: 400;
+}
+
+.pusherT {
+  padding-top: 10%;
+}
+
+.font_style {
+  font-family: 'Oswald', sans-serif;
+  font-size: 20px;
+  color: #ec87e4;
+}
+
+.btn_font {
+  font-family: 'Oswald', sans-serif;
+  font-size: 60px;
+  font-weight: bold;
+}
+</style>
